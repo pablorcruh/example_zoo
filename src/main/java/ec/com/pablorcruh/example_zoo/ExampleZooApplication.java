ba48f7bb4f44f185be1352827f083f8754362c21
pablorcruh <pablorcruh@gmail.com>
2021-03-14 15:46:30 -0500

    initial setup

---
+++ src/main/java/ec/com/pablorcruh/example_zoo/ExampleZooApplication.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.example_zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleZooApplication.class, args);
+    }
+
+}
