18b269dcf93fcc378ef4851fac043d0893f9fc6d
pablorcruh <pablorcruh@gmail.com>
2021-03-15 10:37:11 -0500

    add test profile

--- src/test/java/ec/com/pablorcruh/example_zoo/controller/AnimalControllerTest.java
+++ src/test/java/ec/com/pablorcruh/example_zoo/controller/AnimalControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(AnimalController.class)
+@ActiveProfiles("test")
 public class AnimalControllerTest {
 
     @Autowired
