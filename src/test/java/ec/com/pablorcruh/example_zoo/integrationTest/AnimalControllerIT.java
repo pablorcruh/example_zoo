32c3fa68724900be42c5cdcff8874fa19052ef6f
pablorcruh <pablorcruh@gmail.com>
2021-03-15 18:30:11 -0500

    add testing profile

---
+++ src/test/java/ec/com/pablorcruh/example_zoo/integrationTest/AnimalControllerIT.java
@@ -0,0 +1,10 @@
+package ec.com.pablorcruh.example_zoo.integrationTest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AnimalControllerIT {
+}
