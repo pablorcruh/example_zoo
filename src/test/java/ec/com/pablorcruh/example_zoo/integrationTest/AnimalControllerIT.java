a98294cd7e551f223900936bd7d91358e112d173
pablorcruh <pablorcruh@gmail.com>
2021-03-15 20:50:19 -0500

    add integration test to create animal and retrive animal by id

--- src/test/java/ec/com/pablorcruh/example_zoo/integrationTest/AnimalControllerIT.java
+++ src/test/java/ec/com/pablorcruh/example_zoo/integrationTest/AnimalControllerIT.java
@@ -1,18 +1,20 @@
 package ec.com.pablorcruh.example_zoo.integrationTest;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import ec.com.pablorcruh.example_zoo.model.Animal;
+import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,6 +24,8 @@ public class AnimalControllerIT {
     int randomServerPort;
 
     private TestRestTemplate testRestTemplate;
+
+    private HttpHeaders headers = new HttpHeaders();
 
     @BeforeEach
     public void setup(){
@@ -44,6 +48,31 @@ public class AnimalControllerIT {
         assertThat(secondResult.getStatusCode(), is(HttpStatus.NOT_FOUND));
     }
 
+    @Test
+    public void addNewAnimalToDatabase(){
+        Animal animal = new Animal();
+        animal.setName("animal test");
+        animal.setCategory("category test");
+        HttpEntity<Animal> entity = new HttpEntity<Animal>(animal, headers);
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                createURLWithPort("/api/animals/"),
+                HttpMethod.POST, entity, String.class);
+        String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+        assertTrue(actual.contains("/api/animals"));
+    }
 
 
+    @Test
+    public void retrieveKnownIdAnimalsFromDatabase() throws JSONException {
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                createURLWithPort("/api/animals/3"),
+                HttpMethod.GET, entity, String.class);
+        String expected = "{\"id\":3,\"name\":\"animal test\",\"category\":\"category test\"}";
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    private String createURLWithPort(String uri){
+        return "http://localhost:" + randomServerPort + uri;
+    }
 }
